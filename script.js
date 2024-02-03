f87869e12fc1446118dd878a1219e11a2ed8a1fa
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2024-02-03 19:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let container = document.createElement('div');
+container.style.width = "fit-content";
+
+for(let i = 1;i<=100; i++){
+	let p = document.createElement("p");
+	if(i%15 === 0){
+		
+		p.innerText = `FizzBuzz`;
+		container.appendChild(p);
+	}else if(i%3 === 0){
+		
+		p.innerText = "Fizz"
+		container.appendChild(p);
+	}else if(i%5 === 0){
+		p.innerText = "Buzz";
+		container.apendChild(p);
+	}else {
+		p.innerText = `${i}`;
+		container.appendChild(p);
+	}
+}
